c6dd95a3e222e6376413793187554f2944b7c54b
ccx <1163346669@qq.com>
2024-07-02 21:44:53 +0800

    initial version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Clearning/project
+# Build directory: D:/Clearning/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"max.h"
+#include"min.h"
+
+int max(int a, int b);
+
+int main(int argc, char *argv[]){
+    printf("%d\n", 1);
+    printf("%d\n", 2);
+    printf("%d\n", 3);
+    printf("the max of (7, 8) is: %d\n", max(7, 8));
+    printf("the min of (11, 12) is: %d\n", min(11, 12));
+    return 0;
+
+}
