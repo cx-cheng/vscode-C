592c589552891fe2e2434bb868ae7d115e5ff05e
ccx <1163346669@qq.com>
2024-07-02 21:56:16 +0800

    test

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]){
     printf("%d\n", 1);
     printf("%d\n", 2);
     printf("%d\n", 3);
+    printf("%d\n", 4);
     printf("the max of (7, 8) is: %d\n", max(7, 8));
     printf("the min of (11, 12) is: %d\n", min(11, 12));
     return 0;
